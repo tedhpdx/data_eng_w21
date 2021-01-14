ef5b625bee24f3e33bc21e8edf6ae2ad19df49f5
Ted <herring@pdx.edu>
2021-01-14 11:23:46 -0800

    parsing json

--- project-code/gather_data.py
+++ project-code/gather_data.py
@@ -1,7 +1,15 @@
 import requests
 import json
 
+#json_file_path = '/home/herring/data_eng_w21/project-code/breadcrumb_data.json'
+json_file_path = 'breadcrumb_data.json'
+
 r = requests.get('http://rbi.ddns.net/getBreadCrumbData')
 rj = r.json()
-with open('/home/herring/data_eng_w21/project-code/breadcrumb_data.json', 'w') as outfile:
-    json.dump(rj, outfile)
+first = rj[0]
+second = first['EVENT_NO_TRIP']
+with open(json_file_path, 'w') as outfile:
+        json.dump(rj, outfile)
+
+
+
