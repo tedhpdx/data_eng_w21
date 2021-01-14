261324f810d1bd4ec544834a8a8673b55500212d
Ted Herring <herring@pdx.edu>
2021-01-14 17:18:23 +0000

    gather_data now posts to project-code dir

--- project-code/gather_data.py
+++ project-code/gather_data.py
@@ -3,5 +3,5 @@ import json
 
 r = requests.get('http://rbi.ddns.net/getBreadCrumbData')
 rj = r.json()
-with open('breadcrumb_data.json', 'w') as outfile:
+with open('/home/herring/data_eng_w21/project-code/breadcrumb_data.json', 'w') as outfile:
     json.dump(rj, outfile)
