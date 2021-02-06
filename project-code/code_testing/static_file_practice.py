71a6a64a331474d1f3f441b06dc5de9f4d44776a
Ted <herring@pdx.edu>
2021-02-06 11:32:15 -0800

    postgres practice

--- project-code/code_testing/static_file_practice.py
+++ project-code/code_testing/static_file_practice.py
@@ -1,5 +1,6 @@
 import datetime
 import pandas as pd
+from sqlalchemy import create_engine
 
 #figure 'breadcrumb_single_data' == json.loads(record_value)
 df = pd.read_json('breadcrumb_single_data.json')
@@ -15,4 +16,6 @@ print (str(datetime.timedelta(seconds=time)))
 
 #finish validations
 
+engine = create_engine('postgresql://@34.105.70.119:5432/practice')
+df.to_sql('another_test_trip', engine, if_exists='append', index=False)
 #send to db
