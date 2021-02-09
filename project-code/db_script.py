72715162ac64302c6a2bcfd1366c04cd6e6163ea
Ted Herring <herring@pdx.edu>
2021-02-09 12:09:00 -0800

    added multi

--- project-code/db_script.py
+++ project-code/db_script.py
@@ -43,7 +43,7 @@ def send_to_db(json_package):
 
     breadcrumb_df = pd.DataFrame(columns=['tstamp', 'latitude', 'longitude', 'direction', 'speed', 'trip_id'])
     prev_trip_id = None
-    engine = create_engine('postgresql://herring:Entage1234@34.105.70.119:5432/practice')
+    engine = create_engine('postgresql://:@34.105.70.119:5432/practice')
     for i in range(len(df)):
         print(i)
         trip_ID = df['EVENT_NO_TRIP'][i]
