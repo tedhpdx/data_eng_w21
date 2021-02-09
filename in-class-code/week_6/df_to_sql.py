5aac5aa6c9881766f133300fa3325a77e6bc8793
Ted <herring@pdx.edu>
2021-02-09 11:52:34 -0800

    getting to cli

--- in-class-code/week_6/df_to_sql.py
+++ in-class-code/week_6/df_to_sql.py
@@ -2,4 +2,6 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-df = pd.read_csv('Oregon2015.csv')
+df = pd.read_csv('acs2015_census_tract_data.csv')
+engine = create_engine('postgresql://:@34.105.70.119:5432/storact')
+df.to_sql('censusdata_test', engine, if_exists='append', index=False, method='multi')
